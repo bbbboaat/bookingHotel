1967a4398213819fbc62e699442f38468372cc43
bbbboaat <boatz.b3@gmail.com>
2023-04-01 01:19:17 +0700

    room

--- client/src/components/Booking.jsx
+++ client/src/components/Booking.jsx
@@ -1,8 +1,25 @@
 import Current from "./Current"
+import { Stack ,Box , Flex} from "@chakra-ui/react"
+import Room from "./Room"
+import Deluxe from '../assets/deluxe.jpeg'
+import Suite from '../assets/suite.jpeg'
+import Superior from '../assets/superior.jpeg'
 
 const Booking = () => {
     return (
-        <Current/>  
+        <Stack 
+        as={Box}
+        textAlign={'center'}
+        spaceing={{base: 8 , md:14 }}
+        py = {{base: 20 , md: 36}}>
+        <Current/>
+        <Flex justifyContent={'center'} alignItems={'center'}>
+            <Room room={Superior}/>
+            <Room room={Deluxe}/>
+            <Room room={Suite}/>
+        </Flex>
+        </Stack>
+        
     )
 }
 
---
+++ client/src/components/Room.jsx
@@ -0,0 +1,36 @@
+import { Stack,Box,Image,Button,Text } from "@chakra-ui/react"
+
+const Room = ({room}) => {
+    return(
+        <Box boxSize='lg' mx={2}>
+            <Image src={room} mb={10} />
+            <Text>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora hic laboriosam incidunt, beatae eos eum odio est eius saepe inventore veniam quas! Aperiam repudiandae, optio ipsa obcaecati harum ratione numquam! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores, minus autem necessitatibus quibusdam porro obcaecati ab eos delectus! Vero, hic?
+            </Text>
+            <Stack spacing={0} direction={'row'} align={'center'} justify={'center'} mt={5} >
+            <Button
+              colorScheme={'green'}
+              bg={'purple.400'}
+              rounded={'full'}
+              px={6}
+              _hover={{
+                bg: 'purple.500'
+              }}>
+              Check In
+            </Button>
+            <Button
+              colorScheme={'green'}
+              bg={'purple.400'}
+              rounded={'full'}
+              px={6}
+              _hover={{
+                bg: 'purple.500'
+              }}>
+              Check Out
+            </Button>
+            </Stack>
+        </Box>
+    )
+}
+
+export default Room
